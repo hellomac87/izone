a0eed9993ebb142a8690571e358784f4e7d01871
hellomac87 <hellomac87@gmail.com>
2020-04-18 22:13:57 +0900

    HomeMain

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const App = props => {
-  return <div>App!!</div>;
+const App = (props) => {
+  return <HomeMain />;
 };
+
+const HomeMain = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: royalblue;
+`;
 
 App.propTypes = {};
 
